aa639696b1b51070acf9e9bb57f2f34e46ae6d56
tawana <tawanaworks@gmail.com>
2023-04-25 20:16:21 +0200

    Create classes
    Question 3: ongoing
    
    Created the required classes to do temperature conversion
    
    Three classes are required.
    1 Base class - contains the temperature storage variable
    2 Inhereting classes - perform the conversion activity

---
+++ PG/Programming Q3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Temperature
+{
+private:
+  double temperature = 0;
+
+public:
+  void setTemperature(int tempToSet)
+  {
+    if (temperature != 0)
+    {
+      temperature = 0;
+      temperature += tempToSet;
+    }
+    else
+    {
+      temperature += tempToSet;
+    }
+  }
+  int getTemperature()
+  {
+    return this->temperature;
+  }
+};
+
+class convertTemperature : public Temperature
+{
+public:
+  int toCelsius(){
+    int celsius, inputFahrenheit;
+    cout << "Enter a temperature in fahrenheit: ";
+    cin >> inputFahrenheit;
+    setTemperature(inputFahrenheit);
+    celsius = 5 / 9 * (getTemperature() - 32);
+    return celsius;
+  };
+};
+
+class ToFahrenheit : public Temperature
+{
+public:
+  int toFahrenheit(){
+    int fahrenheit, inputCelsius;
+    cout << "Enter a temperature in fahrenheit: ";
+    cin >> inputCelsius;
+    setTemperature(inputCelsius);
+    fahrenheit = 9 / 5 * getTemperature() + 32;
+    return fahrenheit;
+  };
+};
